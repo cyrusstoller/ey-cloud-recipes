9f6af4286bd39749fddd020282c13afbac0a5ec8
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-03-23 02:49:04 -0700

    Adding thinking sphinx and resque to the recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -5,7 +5,7 @@
 #end
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
-# require_recipe "sphinx"
+require_recipe "sphinx"
 
 #uncomment to turn on memcached
 # require_recipe "memcached"
@@ -38,7 +38,7 @@
 #require_recipe "mongodb"
 
 #uncomment to run the resque recipe
-# require_recipe "resque"
+require_recipe "resque"
 
 #uncomment to run the resque-scheduler recipe
 # require_recipe "resque-scheduler"
